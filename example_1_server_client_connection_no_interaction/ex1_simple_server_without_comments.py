fbf53fc2939195feb6821f9571ef5536ac670a2a
Vivek Tyagi <vivtyagi@gmail.com>
2025-08-04 20:15:09 -0500

    added an uncommented version of server code as an example

---
+++ example_1_server_client_connection_no_interaction/ex1_simple_server_without_comments.py
@@ -0,0 +1,17 @@
+import asyncio
+import websockets
+
+async def handle_client(websocket):
+    print("Client connected")
+
+    message = await websocket.recv()
+    print("Received from client:", message)
+
+    print("Done.")
+
+async def main():
+    async with websockets.serve(handle_client, "localhost", 12345):
+        print("Server is running on ws://localhost:12345")
+        await asyncio.Future()
+
+asyncio.run(main())
